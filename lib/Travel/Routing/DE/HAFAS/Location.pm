00c8fa4f0117197e5daed873ed761302474088b8
Birte Kristina Friesel <derf@finalrewind.org>
2023-11-12 17:31:04 +0100

    Location: Add documentation

--- lib/Travel/Routing/DE/HAFAS/Location.pm
+++ lib/Travel/Routing/DE/HAFAS/Location.pm
@@ -31,3 +31,66 @@ sub new {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Travel::Routing::DE::HAFAS::Location - A single public transit stop
+
+=head1 SYNOPSIS
+
+	printf("Destination: %s  (%8d)\n", $stop->name, $stop->eva);
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+Travel::Routing::DE::HAFAS::Stop describes a HAFAS stop that is part of
+a connection, connection section, or (partial) journey.
+
+=head1 METHODS
+
+=head2 ACCESSORS
+
+=over
+
+=item $stop->name
+
+Stop name, e.g. "Essen Hbf" or "Unter den Linden/B75, Tostedt".
+
+=item $stop->eva
+
+EVA ID, e.g. 8000080.
+
+=back
+
+=head1 DIAGNOSTICS
+
+None.
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Class::Accessor(3pm)
+
+=back
+
+=head1 BUGS AND LIMITATIONS
+
+None known.
+
+=head1 SEE ALSO
+
+Travel::Routing::DE::HAFAS(3pm).
+
+=head1 AUTHOR
+
+Copyright (C) 2023 by Birte Kristina Friesel E<lt>derf@finalrewind.orgE<gt>
+
+=head1 LICENSE
+
+This module is licensed under the same terms as Perl itself.
